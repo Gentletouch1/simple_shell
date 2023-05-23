ab9b8b23d5fc3d4bf0f4eb7f76e0698ac800aa5e
gift <giftmoyo23@gmail.com>
2023-05-23 10:45:10 -0700

    simple shell is cool please compile the files

---
+++ str_funct.c
@@ -0,0 +1,84 @@
+#include "shell.h"
+
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+
+
+/*
+ Returns the length of a string & character length.
+ */
+
+
+int _strlen(const char *s)
+{
+	int length = 0;
+
+	if (!s)
+		return (length);
+	for (length = 0; s[length]; length++)
+		;
+	return (length);
+}
+
+
+/*
+ Copy string pointed to by src, including the terminating null byte, to the buffer.
+ Return: Pointer to dest.
+ */
+
+
+char *_strcpy(char *dest, const char *src)
+{
+	size_t i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*
+Concantenates strings.
+Return: Pointers to destination string.
+ */
+
+char *_strcat(char *dest, const char *src)
+{
+	char *destTemp;
+	const char *srcTemp;
+
+	destTemp = dest;
+	srcTemp =  src;
+
+	while (*destTemp != '\0')
+		destTemp++;
+
+	while (*srcTemp != '\0')
+		*destTemp++ = *srcTemp++;
+	*destTemp = '\0';
+	return (dest);
+}
+
+
+/*
+ Concantenates strings where "n" numbers of bytes are copied from source.
+ Returns pointer to destination of string.
+ */
+
+
+
+
+char *_strncat(char *dest, const char *src, size_t n)
+{
+	size_t dest_len = _strlen(dest);
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
+
